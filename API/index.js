fbb7b114136c4fa7f53eea17a75704bcb0fd027b
Sejal Khanzode <khanzodesejal@gmail.com>
2023-12-04 01:23:56 +0530

    Rest API done

---
+++ API/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors")
+const app = express();
+const plants = require("./routes/plants")
+app.use(cors());
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+const {readdirSync} = require("fs");
+
+
+// readdirSync("./routes").map((file)=>app.use("/",require("./routes/" + file)))
+
+
+app.get("/", (req, res) => {
+  res.send("Main page!");
+});
+
+app.get("/plants", plants);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
---
+++ API/routes/plants.js
@@ -0,0 +1,253 @@
+const express =  require("express");
+const router = express.Router();
+
+const plantData = [
+    {
+      id: 1,
+      title: "Snake Plant (Sansevieria)",
+      price: 29.99,
+      description: "A low-maintenance and air-purifying plant that thrives in indoor conditions. Known for its ability to filter indoor air pollutants.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2023/08/snake-plant-400x300.jpg",
+      rating: {
+        rate: 4.5,
+        count: 150
+      }
+    },
+    {
+      id: 2,
+      title: "Spider Plant (Chlorophytum comosum)",
+      price: 24.95,
+      description: "An adaptable and easy-to-care-for plant with arching leaves. Effective in removing toxins from the air and adding oxygen.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2020/11/indoot-spider-plant-1152x1536.jpg",
+      rating: {
+        rate: 4.2,
+        count: 120
+      }
+    },
+  
+    {
+      id: 3,
+      title: "Aloe Vera",
+      price: 19.99,
+      description: "A succulent plant known for its soothing gel. Apart from its skincare benefits, Aloe Vera also contributes to indoor air purification.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2020/11/aloe-vera-plant-400x300.jpg",
+      rating: {
+        rate: 4.4,
+        count: 100
+      }
+    },
+    {
+      id: 4,
+      title: "Bamboo Palm (Chamaedorea seifrizii)",
+      price: 39.95,
+      description: "An elegant and effective air-purifying plant, particularly known for its ability to remove airborne toxins and add moisture to the air.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2012/12/Bamboo-Palm-400x399.jpg",
+      rating: {
+        rate: 4.6,
+        count: 130
+      }
+    },
+    {
+      id: 5,
+      title: "ZZ Plant (Zamioculcas zamiifolia)",
+      price: 32.99,
+      description: "A resilient and low-maintenance plant with glossy green leaves. Known for its air-purifying properties and ability to thrive in various light conditions.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2016/07/zz-plant.jpg",
+      rating: {
+        rate: 4.8,
+        count: 160
+      }
+    },
+    {
+      id: 6,
+      title: "Peace Lily (Spathiphyllum)",
+      price: 34.99,
+      description: "A beautiful and elegant plant known for its air-purifying qualities. Thrives in low-light conditions and removes common indoor pollutants.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2023/09/peace-lily-400x300.jpg",
+      rating: {
+        rate: 4.7,
+        count: 180
+      }
+    },
+    {
+      id: 7,
+      title: "Rubber Plant (Ficus elastica)",
+      price: 29.99,
+      description: "A popular indoor plant with large, glossy leaves. Effective in removing common indoor pollutants and adding a touch of greenery to your space.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2020/11/ficus-plant-400x533.jpg",
+      rating: {
+        rate: 4.5,
+        count: 120
+      }
+    },
+    {
+      id: 8,
+      title: "Boston Fern (Nephrolepis exaltata)",
+      price: 27.95,
+      description: "A classic fern known for its feathery fronds. This plant not only adds elegance to your space but also helps in improving indoor air quality.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2021/02/boston-fern-400x300.jpg",
+      rating: {
+        rate: 4.3,
+        count: 110
+      }
+    },
+    {
+      id: 9,
+      title: "Chinese Evergreen (Aglaonema)",
+      price: 34.99,
+      description: "An adaptable and visually appealing plant with variegated leaves. Known for its air-purifying qualities and ability to thrive in low-light conditions.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2013/02/Chinese-Evergreen.jpg",
+      rating: {
+        rate: 4.6,
+        count: 150
+      }
+    },
+  
+    {
+      id: 10,
+      title: "Parlor Palm (Chamaedorea elegans)",
+      price: 22.99,
+      description: "A small and elegant palm known for its air-purifying abilities. Perfect for adding a tropical touch to your indoor space.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2015/11/parlor-palm-pot.jpg",
+      rating: {
+        rate: 4.4,
+        count: 130
+      }
+    },
+    {
+      id: 11,
+      title: "Devil's Ivy (Epipremnum aureum)",
+      price: 19.95,
+      description: "A versatile and easy-to-care-for trailing plant. Devil's Ivy is effective in removing indoor pollutants and can thrive in various light conditions.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2021/06/pothos-1-400x300.jpg",
+      rating: {
+        rate: 4.2,
+        count: 100
+      }
+    },
+    {
+      id: 12,
+      title: "Dracaena Marginata",
+      price: 28.99,
+      description: "A tall and slender plant with attractive red-edged leaves. Dracaena Marginata is known for its air-purifying qualities and modern aesthetic.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2018/06/dracaena-seedlings-400x266.jpg",
+      rating: {
+        rate: 4.7,
+        count: 160
+      }
+    },
+    {
+      id: 13,
+      title: "Spider Lily (Hymenocallis)",
+      price: 26.99,
+      description: "A unique and elegant flowering plant known for its spider-like blooms. Spider Lily not only adds beauty but also contributes to indoor air quality.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2021/12/White-tropical-flowers-400x533.jpg",
+      rating: {
+        rate: 4.3,
+        count: 110
+      }
+    },
+    {
+      id: 14,
+      title: "Areca Palm (Dypsis lutescens)",
+      price: 32.95,
+      description: "A popular and versatile palm known for its feathery, arching fronds. Areca Palm is effective in removing indoor pollutants and adds a touch of greenery.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2013/01/areca-palm.jpg",
+      rating: {
+        rate: 4.6,
+        count: 140
+      }
+    },
+    {
+      id: 15,
+      title: "Fiddle Leaf Fig (Ficus lyrata)",
+      price: 39.99,
+      description: "A trendy and visually striking plant with large, fiddle-shaped leaves. Fiddle Leaf Fig is prized for its air-purifying qualities and modern appeal.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2018/02/fiddle-leaf-fig.jpg",
+      rating: {
+        rate: 4.8,
+        count: 170
+      }
+    },
+    {
+      id: 16,
+      title: "Peperomia Obtusifolia",
+      price: 18.99,
+      description: "A compact and easy-to-care-for plant with attractive succulent-like leaves. Peperomia Obtusifolia is known for its air-purifying properties.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2021/07/watermelon-peperomia-plantlet.jpg",
+      rating: {
+        rate: 4.2,
+        count: 95
+      }
+    },
+    {
+      id: 17,
+      title: "Calathea Medallion (Calathea veitchiana)",
+      price: 24.95,
+      description: "An ornamental plant with stunning patterned leaves. Calathea Medallion is not only visually appealing but also helps maintain indoor humidity.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2021/08/calathea-types-400x300.jpg",
+      rating: {
+        rate: 4.5,
+        count: 120
+      }
+    },
+    {
+      id: 18,
+      title: "Nerve Plant (Fittonia)",
+      price: 21.99,
+      description: "A charming plant with colorful, veined leaves. Nerve Plant is suitable for low-light conditions and is valued for its decorative foliage.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2019/08/wilting-fittonia.jpg",
+      rating: {
+        rate: 4.4,
+        count: 110
+      }
+    },
+    {
+      id: 19,
+      title: "Peace Lily (Spathiphyllum Wallisii)",
+      price: 29.99,
+      description: "A classic and elegant plant known for its white blooms and air-purifying qualities. Peace Lily is easy to care for and adapts well to indoor environments.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2016/11/peace-lily-brown-tips-400x561.jpg",
+      rating: {
+        rate: 4.6,
+        count: 130
+      }
+    },
+    {
+      id: 20,
+      title: "Philodendron Birkin",
+      price: 26.95,
+      description: "A striking plant with unique variegated leaves. Philodendron Birkin is appreciated for its low-maintenance care and air-purifying properties.",
+      category: "indoor plants",
+      image: "https://www.gardeningknowhow.com/wp-content/uploads/2023/05/philodendron.jpg",
+      rating: {
+        rate: 4.3,
+        count: 105
+      }
+    }
+  ];
+
+  router.get("/plants",(req,res)=>{
+    res.send(plantData)
+})
+
+module.exports = router
